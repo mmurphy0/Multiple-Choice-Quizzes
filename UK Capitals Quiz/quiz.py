5b962d30cac3fa633121c806a352692c14b67632
Max Murphy <mmurph078@gmail.com>
2025-12-20 12:45:41 +0000

    question 2 complete

--- UK Capitals Quiz/quiz.py
+++ UK Capitals Quiz/quiz.py
@@ -5,25 +5,113 @@ from datetime import datetime
 
 score = 0
 
-def question_1():
+def question_3():
     def correct():
         messagebox.showinfo('Result','Correct!')
         score = score + 1
 
     def incorrect():
+        messagebox.showinfo('Incorrect','The answer is Edinburgh')
+
+
+
+def question_2():
+    def correct():
+        messagebox.showinfo('Result','Correct!')
+        score = score + 1
+        question_3()
+    
+    def incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Cardiff')
+        score = score + 1
+        question_3()
+
+    question2_win = Toplevel()
+    question2_win.geometry('280x90')
+    question2_win.minsize(280,90)
+    question2_win.maxsize(280,90)
+
+    question2_label = tk.Label(
+        question2_win,
+        text='What is the capital of Wales?',
+        font=('Arial',20)
+    )
+    question2_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    question2_button_a = tk.Button(
+        question2_win,
+        text='Swansea',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    question2_button_b = tk.Button(
+        question2_win,
+        text='Flint',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    question2_button_c = tk.Button(
+        question2_win,
+        text='Cardiff',
+        font=('Arial'),
+        width=10,
+        command=correct
+    )
+    question2_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    question2_button_d = tk.Button(
+        question2_win,
+        text='Llandudno',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_d.grid(
+        row=4,
+        column=2
+    )
+
+
+def question_1():
+    def correct():
+        messagebox.showinfo('Result','Correct!')
+        score = score + 1
+        question_2()
+    
+    def incorrect():
         messagebox.showinfo('Result','Incorrect, The answer is London')
+        question_2()
 
     question1_win = Toplevel()
     question1_win.geometry('280x90')
     question1_win.minsize(280,90)
     question1_win.minsize(280,90)
 
-    text = tk.Label(
+    question1_label = tk.Label(
         question1_win,
         text='What is the capital of England?',
         font=('Arial',20)
     )
-    text.grid(
+    question1_label.grid(
         row=1,
         column=1,
         columnspan=2
