a586ac3fc117f4aa9f06102a66ae59e2ba379aee
Max Murphy <mmurph078@gmail.com>
2025-12-23 16:25:20 +0000

    question 2 complete

--- Liverpool FC Quiz/quiz.py
+++ Liverpool FC Quiz/quiz.py
@@ -1,9 +1,87 @@
 import tkinter as tk
 from tkinter import Toplevel, messagebox
 import datetime
-from datetime import date
+from datetime import datetime
 
 score = 0
+
+def question2():
+    def correct():
+        global score
+        messagebox.showinfo('Result','Correct!')
+        score += 1
+
+    def incorrect():
+        messagebox.showinfo('Incorrect','The answer is Anfield')
+
+    global question2_win
+
+    question2_win = Toplevel()
+    question2_win.geometry('280x90')
+    question2_win.minsize('280x90')
+    question2_win.maxsize('280x90')
+    question2_win.title('Liverpool FC Quiz - Question 2')
+
+    question2_label = tk.Label(
+        question2_win,
+        text='What is the name of Liverpools home stadium?',
+        font=('Arial',20)
+    )
+    question2_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    question2_button_a = tk.Button(
+        question2_win,
+        text='Hill Dickinson',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    question2_button_b = tk.Button(
+        question2_win,
+        text='Old Trafford',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    question2_button_c = tk.Button(
+        question2_win,
+        text='Wembley',
+        font=('Arial'),
+        width=10,
+        command=incorrect
+    )
+    question2_button_c.grid(
+        row=4,
+        column=2
+    )
+
+    question2_button_d = tk.Button(
+        question2_win,
+        text='Anfield',
+        font=('Arial'),
+        width=10,
+        command=correct
+    )
+    question2_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question1_win.destroy()
 
 
 def question1():
@@ -14,6 +92,8 @@ def question1():
     
     def incorrect():
         messagebox.showinfo('Result','Incorrect, The answer is 1892')
+
+    global question1_win
 
     question1_win = Toplevel()
     question1_win.geometry('345x90')
