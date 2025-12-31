766fd4ea85512f8fcf1572048abb1847acc50b1d
Max Murphy <mmurph078@gmail.com>
2025-12-31 17:08:21 +0000

    updated file name

--- Premier League Quiz 24/Premier League 24/25 Quiz/quiz.py
+++ Premier League Quiz 24/Premier League 24/25 Quiz/quiz.py
@@ -3,6 +3,21 @@ from tkinter import messagebox, Toplevel
 import datetime
 from datetime import datetime
 
+score = 0
+
+def question1():
+    def correct():
+        global score
+        messagebox.showinfo('Result','Correct!')
+        score += 1
+
+    def incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Liverpool')
+
+    question1_win = Toplevel()
+    question1_win.geometry('280x80+0+0')
+    question1_win.resizable(False,False)
+
 root = tk.Tk()
 root.geometry('257x60+0+0')
 root.resizable(False,False)
@@ -19,7 +34,8 @@ startquiz_button = tk.Button(
     root,
     text='Start Quiz',
     font=('Arial'),
-    width=40
+    width=40,
+    command=question1
 )
 startquiz_button.pack()
 
