d560a54849d2656e00744b5663602b67e7dd7f8b
Max Murphy <mmurph078@gmail.com>
2026-01-01 15:12:12 +0000

    question 2 complete

--- Premier League 24-25 Quiz/quiz.py
+++ Premier League 24-25 Quiz/quiz.py
@@ -5,6 +5,84 @@ from datetime import datetime
 
 score = 0
 
+def question2():
+    def correct():
+        global score
+        messagebox.showinfo('Result','Correct!')
+        score += 1
+
+    def incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Mohamed Salah')
+
+    global question2_win
+
+    question2_win = Toplevel()
+    question2_win.geometry('280x90+0+0')
+    question2_win.resizable(False,False)
+    question2_win.title('Premier League 24/25 Quiz - Q2')
+
+    question2_label = tk.Label(
+        question2_win,
+        text='Which player won the Premier League Golden Boot?',
+        font=('Arial',20)
+    )
+    question2_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    question2_button_a = tk.Button(
+        question2_win,
+        text='Erling Haaland',
+        font=('Arial'),
+        width=15,
+        command=incorrect
+    )
+    question2_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    question2_button_b = tk.Button(
+        question2_win,
+        text='Mohamed Salah',
+        font=('Arial'),
+        width=15,
+        command=correct
+    )
+    question2_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    question2_button_c = tk.Button(
+        question2_win,
+        text='Alexander Isak',
+        font=('Arial'),
+        width=15,
+        command=incorrect
+    )
+    question2_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    question2_button_d = tk.Button(
+        question2_win,
+        text='Bukayo Saka',
+        font=('Arial'),
+        width=15,
+        command=incorrect
+    )
+    question2_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question1_win.destroy()
+
+
 def question1():
     def correct():
         global score
@@ -13,6 +91,8 @@ def question1():
 
     def incorrect():
         messagebox.showinfo('Result','Incorrect, The answer is Liverpool')
+
+    global question1_win
 
     question1_win = Toplevel()
     question1_win.geometry('323x90+0+0')
