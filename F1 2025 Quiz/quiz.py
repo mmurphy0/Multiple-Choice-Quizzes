f5a01d730ac057c2c9b47a5ba46e59ae6e302aef
Max Murphy <mmurph078@gmail.com>
2026-01-11 20:53:16 +0000

    question 4 complete

--- F1 2025 Quiz/quiz.py
+++ F1 2025 Quiz/quiz.py
@@ -3,6 +3,83 @@ from tkinter import Toplevel, messagebox
 import datetime
 from datetime import datetime
 
+def question4():
+    def q4_correct():
+        correct()
+        question5()
+
+    def q4_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Max Verstappen')
+
+    global question4_win
+
+    question4_win = Toplevel()
+    question4_win.geometry('320x90+0+0')
+    question4_win.resizable(False,False)
+    question4_win.title('F1 2025 Quiz - Q4')
+
+    question4_label = tk.Label(
+        question4_win,
+        text="Who was 2nd in the Driver's Championship?",
+        font=('Arial',20)
+    )
+    question4_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    question4_button_a = tk.Button(
+        question4_win,
+        text='Charles Leclerc',
+        font=('Arial'),
+        width=15,
+        command=q4_incorrect
+    )
+    question4_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    question4_button_b = tk.Button(
+        question4_win,
+        text='Max Verstappen',
+        font=('Arial'),
+        width=15,
+        command=q4_correct
+    )
+    question4_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    question4_button_c = tk.Button(
+        question4_win,
+        text='Oscar Piastri',
+        font=('Arial'),
+        width=15,
+        command=q4_incorrect
+    )
+    question4_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    question4_button_d = tk.Button(
+        question4_win,
+        text='George Russell',
+        font=('Arial'),
+        width=15,
+        command=q4_incorrect
+    )
+    question4_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question3_win.destroy()
+
+
 def question3():
     def q3_correct():
         correct()
