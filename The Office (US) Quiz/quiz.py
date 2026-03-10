8e4184a93b623c58ccd72c6b538914940bf48bb3
Max Murphy <mmurph078@gmail.com>
2026-03-10 20:54:28 +0000

    question 1 complete

--- The Office (US) Quiz/quiz.py
+++ The Office (US) Quiz/quiz.py
@@ -5,6 +5,79 @@ from tkinter import messagebox
 import time
 from time import strftime
 
+def question1():
+    def q1_correct():
+        correct()
+        question2()
+
+    def q1_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Dunder Mifflin')
+        question2()
+
+    question1_win = Toplevel()
+    question1_win.geometry('0+0')
+    question1_win.resizable(False,False)
+    question1_win.title('The Office (US) Quiz - Q1')
+
+    q1_label = tk.Label(
+        question1_win,
+        text='What is the name of the paper company?',
+        font=('Arial',20)
+    )
+    q1_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q1_button_a = tk.Button(
+        question1_win,
+        text='Penn Paper',
+        font=('Arial'),
+        width=15,
+        command=q1_incorrect
+    )
+    q1_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q1_button_b = tk.Button(
+        question1_win,
+        text='Scranton Paper Co.',
+        font=('Arial'),
+        width=15,
+        command=q1_incorrect
+    )
+    q1_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q1_button_c = tk.Button(
+        question1_win,
+        text='Dunder Mifflin',
+        font=('Arial'),
+        width=15,
+        command=q1_correct
+    )
+    q1_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q1_button_d = tk.Button(
+        question1_win,
+        text='Northeast Paper',
+        font=('Arial'),
+        width=15,
+        command=q1_incorrect
+    )
+    q1_button_d.grid(
+        row=4,
+        column=2
+    )
+
 def correct():
     messagebox.showinfo('Result','Correct!')
     global score
@@ -29,8 +102,8 @@ startquiz_button = tk.Button(
     root_win,
     text='Start',
     font=('Arial'),
-    width=20
-    # command=question1
+    width=20,
+    command=question1
 )
 startquiz_button.pack()
 
