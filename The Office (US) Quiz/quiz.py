4a769dad2a3f3518631314dd9cb28fb646ecf296
Max Murphy <mmurph078@gmail.com>
2026-03-21 20:55:24 +0000

    question 18 complete

--- The Office (US) Quiz/quiz.py
+++ The Office (US) Quiz/quiz.py
@@ -5,6 +5,57 @@ from tkinter import messagebox
 import time
 from time import strftime
 
+def question18(question17_win):
+    def q18_correct():
+        correct()
+        question19(question18_win)
+
+    def q18_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is True')
+        question19(question18_win)
+
+    question18_win = Toplevel()
+    question18_win.geometry('+0+0')
+    question18_win.resizable(False,False)
+    question18_win.title('The Office (US) Quiz - Q18')
+
+    q18_label = tk.Label(
+        question18_win,
+        text='Oscar Martinez works as an accountant?',
+        font=('Arial',20)
+    )
+    q18_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q18_button_a = tk.Button(
+        question18_win,
+        text='True',
+        font=('Arial'),
+        width=15,
+        command=q18_correct
+    )
+    q18_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q18_button_b = tk.Button(
+        question18_win,
+        text='False',
+        font=('Arial'),
+        width=15,
+        command=q18_incorrect
+    )
+    q18_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    question17_win.destroy()
+
 def question17(question16_win):
     def q17_correct():
         correct()
