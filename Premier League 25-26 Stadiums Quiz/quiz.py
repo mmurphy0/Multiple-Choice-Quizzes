dde4b86197738ad352742423daf620438db64b9b
Max Murphy <mmurph078@gmail.com>
2026-04-06 16:44:28 +0100

    question 5 complete

--- Premier League 25-26 Stadiums Quiz/quiz.py
+++ Premier League 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,81 @@ from tkinter import messagebox
 
 from time import strftime
 
+def question5(question4_win):
+    def q5_correct():
+        correct()
+        question6(question5_win)
+
+    def q5_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is Villa Park')
+        question6(question5_win)
+
+    question5_win = Toplevel()
+    question5_win.geometry('+0+0')
+    question5_win.resizable(False,False)
+    question5_win.title('Premier League 25/26 Quiz - Q5')
+
+    q5_label = tk.Label(
+        question5_win,
+        text='What is the Aston Villa stadium called?',
+        font=('Arial',20)
+    )
+    q5_label.grid(
+        row=1,
+        columm=1,
+        columnspan=2
+    )
+
+    q5_button_a = tk.Button(
+        question5_win,
+        text='Emirates Stadium',
+        font=('Arial'),
+        width=20,
+        command=q5_incorrect
+    )
+    q5_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q5_button_b = tk.Button(
+        question5_win,
+        text='Elland Road',
+        font=('Arial'),
+        width=20,
+        command=q5_incorrect
+    )
+    q5_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q5_button_c = tk.Button(
+        question5_win,
+        text='Villa Park',
+        font=('Arial'),
+        width=20,
+        command=q5_correct
+    )
+    q5_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q5_button_d = tk.Button(
+        question5_win,
+        text='Gtech Community Stadium',
+        font=('Arial'),
+        width=20,
+        command=q5_incorrect
+    )
+    q5_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question4_win.destroy()
+
 def question4(question3_win):
     def q4_correct():
         correct()
