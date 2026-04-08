1d28d119c8b3990edc6de3df0105f81fc4497e2d
Max Murphy <mmurph078@gmail.com>
2026-04-08 22:21:15 +0100

    question 8 complete

--- Premier League 25-26 Stadiums Quiz/quiz.py
+++ Premier League 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,81 @@ from tkinter import messagebox
 
 from time import strftime
 
+def question8(question7_win):
+    def q8_correct():
+        correct()
+        question9(question8_win)
+
+    def q8_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is the Hill Dickinson Stadium')
+        question9(question8_win)
+
+    question8_win = Toplevel()
+    question8_win.geometry('+0+0')
+    question8_win.resizable(False,False)
+    question8_win.title('Premier League 25/26 Stadiums Quiz - Q8')
+
+    q8_label = tk.Label(
+        question8_win,
+        text='What is the Everton stadium called?',
+        font=('Arial',20)
+    )
+    q8_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q8_button_a = tk.Button(
+        question8_win,
+        text='Hill Dickinson Stadium',
+        font=('Arial'),
+        width=20,
+        command=q8_correct
+    )
+    q8_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q8_button_b = tk.Button(
+        question8_win,
+        text='Goodison Park',
+        font=('Arial'),
+        width=20,
+        command=q8_incorrect
+    )
+    q8_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q8_button_c = tk.Button(
+        question8_win,
+        text='Molineux Stadium',
+        font=('Arial'),
+        width=20,
+        command=q8_incorrect
+    )
+    q8_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q8_button_d = tk.Button(
+        question8_win,
+        text='Turf Moor',
+        font=('Arial'),
+        width=20,
+        command=q8_incorrect
+    )
+    q8_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question7_win.destroy()
+
 def question7(question6_win):
     def q7_correct():
         correct()
