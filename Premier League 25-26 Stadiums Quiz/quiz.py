e0a2bcc14ef0435151e3c5a069da7badb92d25e5
Max Murphy <mmurph078@gmail.com>
2026-04-14 17:31:14 +0100

    question 13 complete

--- Premier League 25-26 Stadiums Quiz/quiz.py
+++ Premier League 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,81 @@ from tkinter import messagebox
 
 from time import strftime
 
+def question13(question12_win):
+    def q13_correct():
+        correct()
+        question14(question13_win)
+    
+    def q13_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is the Vitality Stadium')
+        question14(question13_win)
+
+    question13_win = Toplevel()
+    question13_win.geometry('+0+0')
+    question13_win.resizable(False,False)
+    question13_win.title('Premier League 25/26 Stadiums Quiz - Q13')
+
+    q13_label = tk.Label(
+        question13_win,
+        text='What is the Bournemouth stadium called?',
+        font=('Arial',20)
+    )
+    q13_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q13_button_a = tk.Button(
+        question13_win,
+        text='American Express Stadium',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q13_button_b = tk.Button(
+        question13_win,
+        text='Old Trafford',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q13_button_c = tk.Button(
+        question13_win,
+        text='City Ground',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q13_button_d = tk.Button(
+        question13_win,
+        text='Vitality Stadium',
+        font=('Arial'),
+        width=20,
+        command=q13_correct
+    )
+    q13_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question12_win.destroy()
+
 def question12(question11_win):
     def q12_correct():
         correct()
