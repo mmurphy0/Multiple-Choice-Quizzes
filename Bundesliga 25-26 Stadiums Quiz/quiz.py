5ae14d9414baa37b6c4b442fb1ba5a3cbe3022f9
Max Murphy <mmurph078@gmail.com>
2026-05-16 21:01:44 +0100

    question 14 complete

--- Bundesliga 25-26 Stadiums Quiz/quiz.py
+++ Bundesliga 25-26 Stadiums Quiz/quiz.py
@@ -4,13 +4,88 @@ from tkinter import Toplevel
 
 from time import strftime
 
+def question14(question13_win):
+    def q14_correct():
+        correct()
+        question15(question14_win)
+
+    def q14_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is the Volksparkstadion')
+        question15(question14_win)
+
+    question14_win = Toplevel()
+    question14_win.geometry('+0+0')
+    question14_win.resizable(False,False)
+    question14_win.title('Bundesliga 25-26 Stadiums Quiz - Q14')
+
+    q14_label = tk.Label(
+        question14_win,
+        text='What is the Hamburg Stadium called?',
+        font=('Arial',20)
+    )
+    q14_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q14_button_a = tk.Button(
+        question14_win,
+        text='Signal Iduna Park',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q14_button_b = tk.Button(
+        question14_win,
+        text='Volksparkstadion',
+        font=('Arial'),
+        width=20,
+        command=q14_correct
+    )
+    q14_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q14_button_c = tk.Button(
+        question14_win,
+        text='BayArena',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q14_button_d = tk.Button(
+        question14_win,
+        text='Mewa Arena',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question13_win.destroy()
+
 def question13(question12_win):
     def q13_correct():
         correct()
         question14(question13_win)
 
     def q13_incorrect():
-        messagebox.showinfo('Result','Incorrect, The answer is the Borussia-Park')
+        messagebox.showinfo('Result','Incorrect, The answer is Borussia-Park')
         question14(question13_win)
 
     question13_win = Toplevel()
