26f6a4f1128e10133450dc4ba78f2e5294aa50ad
Max Murphy <mmurph078@gmail.com>
2026-05-16 20:46:33 +0100

    question 13 complete

--- Bundesliga 25-26 Stadiums Quiz/quiz.py
+++ Bundesliga 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,81 @@ from tkinter import Toplevel
 
 from time import strftime
 
+def question13(question12_win):
+    def q13_correct():
+        correct()
+        question14(question13_win)
+
+    def q13_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is the Borussia-Park')
+        question14(question13_win)
+
+    question13_win = Toplevel()
+    question13_win.geometry('+0+0')
+    question13_win.resizable(False,False)
+    question13_win.title('Bundesliga 25-26 Stadiums Quiz - Q13')
+
+    q13_label = tk.Label(
+        question13_win,
+        text='What is the Mönchengladbach stadium called?',
+        font=('Arial',20)
+    )
+    q13_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q13_button_a = tk.Button(
+        question13_win,
+        text='Allianz Arena',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q13_button_b = tk.Button(
+        question13_win,
+        text='WWk Arena',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q13_button_c = tk.Button(
+        question13_win,
+        text='PreZero Arena',
+        font=('Arial'),
+        width=20,
+        command=q13_incorrect
+    )
+    q13_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q13_button_d = tk.Button(
+        question13_win,
+        text='Borussia-Park',
+        font=('Arial'),
+        width=20,
+        command=q13_correct
+    )
+    q13_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question12_win.destroy()
+
 def question12(question11_win):
     def q12_correct():
         correct()
