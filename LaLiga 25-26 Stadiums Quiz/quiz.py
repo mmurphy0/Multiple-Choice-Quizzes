f7fd21a694c0a12d961b02014aa941faf59b09ea
Max Murphy <mmurph078@gmail.com>
2026-06-13 23:37:56 +0100

    question 14 complete

--- LaLiga 25-26 Stadiums Quiz/quiz.py
+++ LaLiga 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,81 @@ from tkinter import messagebox
 
 from time import strftime
 
+def question14(question13_win):
+    def q14_correct():
+        correct()
+        question15(question14_win)
+
+    def q14_incorrect():
+        messagebox.showinfo('Result','Incorrect, The answer is the Mendizorrotza Stadium')
+        question15(question14_win)
+
+    question14_win = Toplevel()
+    question14_win.geometry('+0+0')
+    question14_win.resizable(False,False)
+    question14_win.title('LaLiga 25-26 Stadiums Quiz - Q14')
+
+    q14_label = tk.Label(
+        question14_win,
+        text='What is the Alavés Stadium called?',
+        font=('Arial',20)
+    )
+    q14_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q14_button_a = tk.Button(
+        question14_win,
+        text='Vallecas Stadium',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q14_button_b = tk.Button(
+        question14_win,
+        text='Mendizorrotza Stadium',
+        font=('Arial'),
+        width=20,
+        command=q14_correct
+    )
+    q14_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q14_button_c = tk.Button(
+        question14_win,
+        text='RCDE Stadium',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q14_button_d = tk.Button(
+        question14_win,
+        text='Bernabéu',
+        font=('Arial'),
+        width=20,
+        command=q14_incorrect
+    )
+    q14_button_d.grid(
+        row=4,
+        column=2
+    )
+
+    question15_win.destroy()
+
 def question13(question12_win):
     def q13_correct():
         correct()
