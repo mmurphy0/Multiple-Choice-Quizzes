0d46fc424b10f1050bef5dbf791631d53fb6bfa2
Max Murphy <mmurph078@gmail.com>
2026-07-05 16:40:34 +0100

    question 1 complete

--- Ligue 1 25-26 Stadiums Quiz/quiz.py
+++ Ligue 1 25-26 Stadiums Quiz/quiz.py
@@ -4,6 +4,79 @@ from tkinter import Toplevel
 
 from time import strftime
 
+def question1():
+    def q1_correct():
+        correct()
+        question2(question1_win)
+
+    def q1_incorrect():
+        messagebox.showinfo('Result','The answer is the Parc des Princes')
+        question2(question1_win)
+
+    question1_win = Toplevel()
+    question1_win.geometry('+0+0')
+    question1_win.resizable(False,False)
+    question1_win.title('Ligue 1 25-26 Stadiums Quiz')
+
+    q1_label = tk.Label(
+        question1_win,
+        text='What is the PSG Stadium called?',
+        font=('Arial',20)
+    )
+    q1_label.grid(
+        row=1,
+        column=1,
+        columnspan=2
+    )
+
+    q1_button_a = tk.Button(
+        question1_win,
+        text='Stade Bollaert-Delelis',
+        font=('Arial'),
+        width=20,
+        command=q1_incorrect
+    )
+    q1_button_a.grid(
+        row=3,
+        column=1
+    )
+
+    q1_button_b = tk.Button(
+        question1_win,
+        text='Parc des Princes',
+        font=('Arial'),
+        width=20,
+        command=q1_correct
+    )
+    q1_button_b.grid(
+        row=3,
+        column=2
+    )
+
+    q1_button_c = tk.Button(
+        question1_win,
+        text='Groupama Stadium',
+        font=('Arial'),
+        width=20,
+        command=q1_incorrect
+    )
+    q1_button_c.grid(
+        row=4,
+        column=1
+    )
+
+    q1_button_d = tk.Button(
+        question1_win,
+        text='Orange Vélodrome',
+        font=('Arial'),
+        width=20,
+        command=q1_incorrect
+    )
+    q1_button_d.grid(
+        row=4,
+        column=2
+    )
+
 def correct():
     global score
     score += 1
